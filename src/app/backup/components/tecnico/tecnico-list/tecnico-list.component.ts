2127f5746ee80047a2f689af65550346231a9904
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-10-17 00:06:29 -0300

    commit teste

---
+++ src/app/backup/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = []
+  tecnicoParaDeletar: Tecnico;
+  isModalVisible: boolean = false;
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private service: TecnicoService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Tecnico>(resposta);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openDeleteModal(tecnico: Tecnico) {
+    this.tecnicoParaDeletar = tecnico;
+    this.isModalVisible = true;
+  }
+
+  handleConfirmation(confirmed: boolean) {
+    if (confirmed) {
+      this.deleteTecnico();
+    }
+    this.isModalVisible = false;
+  }
+
+  deleteTecnico() {
+    this.service.delete(this.tecnicoParaDeletar.id).subscribe(() => {
+      this.toastr.success('Técnico deletado com sucesso', 'Sucesso');
+      this.findAll();
+    }, err => {
+      this.toastr.error('Erro ao deletar o técnico', 'Erro');
+    });
+  }
+}
---
+++ src/app/backup/config/api.config.ts
@@ -0,0 +1,3 @@
+export const API_CONFIG = {
+   baseUrl : 'https://helpdesk-java-ec2b9324d517.herokuapp.com'
+}
