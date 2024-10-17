7377f5c2a2efaf2365c0d253a3ed12ffffd205f6
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-10-17 14:25:11 -0300

    Ajustando css de chamado-create e removendo regra no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,17 +11,7 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        const headerContainer = document.querySelector('.header-container');
-
-        if (event.url === '/chamados/create') {
-          this.renderer.setStyle(headerContainer, 'width', '157%');
-        } else {
-          this.renderer.setStyle(headerContainer, 'width', '94%'); 
-        }
-      }
-    });
+  
   }
 
 }
